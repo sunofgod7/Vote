59d7b140e7826b1832e469e4a7ed6505680a2864
sunofgod7 <mehransunofgod7@gmail.com>
2023-11-09 22:20:13 +0330

    Prepare files and dependencies

--- src/Vote_Token.sol
+++ src/Vote_Token.sol
@@ -7,7 +7,9 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract Erc20VoteProject is ERC20, Ownable, ERC20Permit, ERC20Votes {
-    constructor(address initialOwner)
+    constructor(
+        address initialOwner
+    )
         ERC20("Erc20VoteProject", "EVP")
         Ownable(initialOwner)
         ERC20Permit("Erc20VoteProject")
@@ -19,21 +21,17 @@ contract Erc20VoteProject is ERC20, Ownable, ERC20Permit, ERC20Votes {
         _mint(to, amount);
     }
 
-    // The following functions are overrides required by Solidity.
-
-    function _update(address from, address to, uint256 value)
-        internal
-        override(ERC20, ERC20Votes)
-    {
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Votes) {
         super._update(from, to, value);
     }
 
-    function nonces(address owner)
-        public
-        view
-        override(ERC20Permit, Nonces)
-        returns (uint256)
-    {
+    function nonces(
+        address owner
+    ) public view override(ERC20Permit, Nonces) returns (uint256) {
         return super.nonces(owner);
     }
 }
